4dc6c1a911c21cc09e5bbc4bb7525fe561e79eca
Your Name <you@example.com>
2022-12-23 16:03:12 +0700

    completed lab9-3

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -2,7 +2,7 @@
 "Enter your age: "
 "Enter your height: "
 "Enter your bounty: "
-"Your character = "
+character = "
 "Zoro"
 "Sanji"
 "Usopp"
@@ -11,3 +11,41 @@
 "Brook"
 "Jinbe"
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int age,height,bounty;
+    string character;
+    cout<<"Enter your age: ";
+    cin>>age;
+    if (age<=25){
+        cout<<"Enter your height: ";
+        cin>>height;
+        if (height<100)
+            character = "Chopper";
+        else if (height<180)
+            character = "Usopp";
+        else {
+            cout<<"Enter your bounty: ";
+            cin>>bounty;
+            if (bounty>1.1e9)
+                character = "Zoro";
+            else
+                character = "Sanji";
+        }
+    }
+    else if (age<=60) {
+        cout<<"Enter your bounty: ";
+        cin>>bounty;
+        if (bounty>5e8)
+            character = "Jinbe";
+        else
+            character = "Franky";
+    }
+    else
+        character = "Brook";
+    cout<<"Your character = "<<character;
+}
